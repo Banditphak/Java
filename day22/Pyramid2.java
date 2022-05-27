7e50d74aa2b3b0b3619f6a861f37faaeae7c9a19
Banditphak <banditphak@hotmail.com>
2022-05-27 00:19:24 -0400

    day22

---
+++ day22/Pyramid2.java
@@ -0,0 +1,27 @@
+package day22;
+//****************** Final Home work ****************************
+public class Pyramid2 {
+	public static void main(String[] args) {
+		int num = 5;
+		
+		/*
+		 *     #
+		 *    ##
+		 *   ###
+		 *  ####
+		 * #####    
+		 */
+		
+		String p = "#";
+		for (int i = 0; i < num; i++) {
+			String space = "";
+			for (int j = 0; j < num - 1 - i; j++) {
+				space += " ";
+			}
+			
+			System.out.println(space + p);
+			p += "#";
+		
+		}
+	}
+}
