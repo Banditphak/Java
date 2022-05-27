0955343a82ff86d87ac6b844a832e49ce9d0c8fe
Banditphak <banditphak@hotmail.com>
2022-05-27 11:46:37 -0400

    day23

---
+++ day23/CarDemo2.java
@@ -0,0 +1,15 @@
+  //******import form  Day 23 for Day 24******
+package day23;
+
+import day24.Car;
+
+public class CarDemo2 {
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.model = "BMW";
+		// car.year = 2021; // not visible
+		// car.price = 35000; // not visible
+		// car.color = "black"; // not visible
+		car.printDetails();
+	}
+}
